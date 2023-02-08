4f03be5b48b76fa982c665349e7c19306a2f8841
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-02-08 10:35:58 -0500

    Mise a jour bloginfo

--- header.php
+++ header.php
@@ -8,4 +8,9 @@
     <?php wp_head();?>
 </head>
 <body>
+    <header>
+        <h1><?php bloginfo("name"); ?></h1>
+        <h2><?php bloginfo("description"); ?></h2>
+        <h2><?php bloginfo("url"); ?></h2>
+    </header>
     
