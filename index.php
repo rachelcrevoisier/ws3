c4e998671324f6306b9de287687202c661de45fe
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-02-09 16:27:43 -0500

    Modification du permalien

--- index.php
+++ index.php
@@ -2,16 +2,21 @@
 
 <?php get_header();?>
 <main>
-    <h3>Index.php</h3>
+
 <?php 
 if (have_posts()):
     while(have_posts()):the_post();
-        the_title("<h1>", "</h1>");
-        the_excerpt();
+        //the_title("<h1>", "</h1>");
+        //the_permalink();?>
+        <h1><a href="<?php the_permalink(); ?>"><?=get_the_title() ?></a></h1>
+        <?php
+        //the_excerpt();
+        wp_trim_words(the_excerpt(),4);
+        //echo wp_trim_words(get_the_excerpt(),14);
     endwhile;
 endif;
 ?>
-
+ 
 
 </main>
 <?php get_footer();?>
