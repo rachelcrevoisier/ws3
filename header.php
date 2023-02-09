fdfb79f5e0a1814c529406975c30f1d79ac9a10d
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-02-09 15:16:25 -0500

    Ajout du nom du site avec le lien dans le header

--- header.php
+++ header.php
@@ -12,5 +12,7 @@
         <h1><?php bloginfo("name"); ?></h1>
         <h2><?php bloginfo("description"); ?></h2>
         <h2><?php bloginfo("url"); ?></h2>
+
+        <h1><a href="<?php bloginfo("url"); ?>" target="_blank"><?php bloginfo("name"); ?></a></h1>
     </header>
     
