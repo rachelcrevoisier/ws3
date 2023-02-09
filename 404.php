7fd961f257acb2798c2391e3dfe0d8d50418ecab
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-02-09 16:25:15 -0500

    creation de la page 404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<!-- Modele.php représente le modèle par défaut du theme -->
+
+<?php get_header();?>
+<main>
+<code>Code 404</code>
+<h1> Erreur 404 - La page n'existe pas</h1>
+
+</main>
+<?php get_footer();?>
