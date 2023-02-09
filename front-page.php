20f66bbf9e472fececdb61b6f80cc6d71d2be112
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-02-09 16:26:45 -0500

    Creation de la page front-page qui remplace l'index

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<!-- Modele.php représente le modèle par défaut du theme -->
+
+<?php get_header();?>
+<main>
+
+<?php 
+if (have_posts()):
+    while(have_posts()):the_post();
+        //the_title("<h1>", "</h1>");
+        //the_permalink();?>
+        <h1><a href="<?php the_permalink(); ?>"><?=get_the_title() ?></a></h1>
+        <?php
+        //the_excerpt();
+        wp_trim_words(the_excerpt(),4);
+        //echo wp_trim_words(get_the_excerpt(),14);
+    endwhile;
+endif;
+?>
+ 
+
+</main>
+<?php get_footer();?>
