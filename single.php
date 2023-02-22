4a6b1e8f2b615e78ca52d7de5593b87f32f6f67b
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-02-22 08:43:09 -0500

    mise a jour

--- single.php
+++ single.php
@@ -1,17 +1,17 @@
-<!-- Modele.php représente le modèle par défaut du theme -->
-
-<?php get_header();?>
+<?php
+/**
+    Modèle index.php représente le modèle par défaut du thème
+*/
+get_header() ?>
 <main>
-    <a href="<?php bloginfo("url"); ?>">
-        <?php 
+    <h3>index.php</h3>
+    <?php 
 if (have_posts()):
-    
-        the_title("<h1>", "</h1>");
-        the_content();
-    
+   while(have_posts()): the_post();
+        the_title('<h1>','</h1>');
+        the_content();  
+    endwhile;    
 endif;
 ?>
-    </a>
-
 </main>
-<?php get_footer();?>
+<?php get_footer(); ?>
