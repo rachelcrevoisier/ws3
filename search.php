b5158ddd59d74e166d16eaf2c75fc4c130fe2c6a
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-02-23 15:31:50 -0500

    Mise a jour de la mise en page pour la recherche et la categorie

--- search.php
+++ search.php
@@ -2,24 +2,21 @@
 
 <?php get_header();?>
 <main>
-    <code>Page search</code>
     <section class="recherche">
-
-
-
         <?php 
-if (have_posts()):
-    while(have_posts()):the_post();
-        //the_title("<h1>", "</h1>");
-        //the_permalink();?>
+            if (have_posts()):
+                while(have_posts()):the_post();
+                    //the_title("<h1>", "</h1>");
+                    //the_permalink();?>
         <article>
-            <h1><a href="<?php the_permalink(); ?>"><?=get_the_title() ?></a></h1>
-            <?=wp_trim_words(the_excerpt(),4);?>
+            <h5><a href="<?php the_permalink(); ?>"><?=get_the_title() ?></a></h5>
+            <?=wp_trim_words(the_excerpt(),50);?>
         </article>
+        <hr>
         <?php
-            endwhile;
-            endif;
-            ?>
+                        endwhile;
+                        endif;
+                        ?>
 
     </section>
 </main>
