06f6782106ea21442e034ceb7375a43cb274f6c1
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-02-23 15:29:03 -0500

    Mise a jour category avec le texte du prof

--- category.php
+++ category.php
@@ -1,23 +1,25 @@
 <!-- Modele.php représente le modèle par défaut du theme -->
 
 <?php get_header();?>
-<main>
+<main class="site__main">
     <section class="blocflex">
-        <?php 
-    /**
-     * Category : permet d'afficher une categorie d'article
-    */
-
-if (have_posts()):
-    while(have_posts()):the_post();?>
+        <?php
+      $category = get_queried_object();
+      $args = array(
+         'category_name' => $category->slug,
+         'orderby' => 'title',
+         'order' => 'ASC'
+      );
+      $query = new WP_Query( $args );
+      if ( $query->have_posts() ) :
+         while ( $query->have_posts() ) : $query->the_post(); ?>
         <article>
-            <h3><a href="<?php the_permalink(); ?>"><?=get_the_title() ?></a></h3>
-            <p><?=wp_trim_words(the_excerpt(),4);?></p>
-
+            <h2><a href="<?php the_permalink(); ?>"> <?= get_the_title(); ?></a></h2>
+            <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
         </article>
-
-        <?php endwhile; endif; ?>
-
+        <?php endwhile; ?>
+        <?php endif;
+      wp_reset_postdata();?>
     </section>
 </main>
 <?php get_footer();?>
