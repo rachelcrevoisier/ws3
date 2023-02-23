b0cd9564dc40d044c30d734ff4a0f64078cb7a27
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-02-23 14:38:27 -0500

    Ajout de la fonction recherche

--- header.php
+++ header.php
@@ -17,6 +17,8 @@
                 'menu' => 'entete',
                 'container' => 'nav'
             )); ?>
+
+            <?=get_search_form();?>
         </section>
         <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1>
         <h2><?php  bloginfo('description'); ?></h2>
